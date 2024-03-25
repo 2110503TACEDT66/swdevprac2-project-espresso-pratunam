768800259bfba2c460181e53a670a40aa9669d3a
VizKey01 <micmin0101@gmail.com>
2024-03-26 00:20:22 +0700

    edit transition

--- src/components/menuPopup.tsx
+++ src/components/menuPopup.tsx
@@ -9,22 +9,24 @@ interface MenuPopupProps {
 const Menu = ({ isMenuOpen, onMenuClose }: MenuPopupProps) => {
   return (
     <div
-      className={`absolute w-screen h-screen top-full right-0  bg-opacity-90 backdrop-blur-md shadow-md rounded-lg transition-transform  duration-1000 ${
+      className={`absolute w-screen h-screen top-full right-0  bg-opacity-100 backdrop-blur-[16px] shadow-md rounded-lg transition-transform  duration-1000 ${
         isMenuOpen ? "translate-y-0 opacity-100" : "translate-y-full opacity-80"
       }`}
     >
       <ul className="py-32 px-4 text-white font-normal">
         <li className="flex justify-center relative text-center w-full mb-4 translate-y-0 translate-x-0">
-          <Link href="/menu-item-1">
-            <span className="text-9xl text-inherit ml-[80px] inline-block transition-all duration-500 hover:text-[#004bfa] hover:font-semibold">
-                Menu Item 1
+          <Link href="/menu-item-1" className={`transition-all duration-[1500ms] ${
+                isMenuOpen ? "translate-x-0" : "translate-x-full"}`}>
+            <span className={`text-9xl text-inherit ml-[200px] inline-block transition-all duration-[400ms] hover:text-[#004bfa] hover:font-semibold`}>
+                <span className="text-4xl">01</span> Car Rental
             </span>
             </Link>
         </li>
         <li className="flex justify-center relative text-center w-full mb-4 translate-y-0 translate-x-0">
-          <Link href="/menu-item-2">
-            <span className="text-9xl text-inherit ml-[-200px] inline-block transition-all duration-500 hover:text-[#004bfa] hover:font-semibold">
-                Menu Item 2
+          <Link href="/menu-item-2 " className={`transition-all duration-[2000ms] ${
+                isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
+            <span className={`text-9xl text-inherit ml-[-200px] inline-block duration-[400ms]  hover:text-[#004bfa] hover:font-semibold`}>
+                Booking List  <span className="text-4xl">02</span>
             </span>
             </Link>
         </li>
