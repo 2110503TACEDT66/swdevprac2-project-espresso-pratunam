4322e2bc4ea6b1a0c1e3486071755c3804441ddb
understann <nattapon.how@gmail.com>
2024-03-25 20:54:01 +0700

    edit a bit

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Banner = () => {
     return(
-        <div className="h-screen w-screen">
+        <div className="h-screen w-screen relative">
         <Image
           src="/img/landing.jpg"
           layout="fill"
@@ -11,8 +11,8 @@ const Banner = () => {
         />
         <div className="absolute inset-0 bg-black opacity-70"></div>
         <div className="absolute w-full h-full flex flex-col items-center justify-center text-white">
-          <h1 className="font-semibold text-7xl mb-3">Espresso Pratunam</h1>
-          <p className="font-light text-5xl mb-10">Car Rental</p>
+          <h1 className="font-semibold text-8xl mb-3">Espresso Pratunam</h1>
+          <p className="font-light text-6xl mb-10">Car Rental</p>
           <button className="bg-white text-black font-bold py-3 px-6 text-xl rounded-full hover:shadow-[0_0_60px_10px_rgba(255,255,255,0.2)]">
             Book now
           </button>
