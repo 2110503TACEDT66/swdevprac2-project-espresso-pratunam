c740727881a47f047065289c5863786b0c634b0f
gg05t <144322678+gg05t@users.noreply.github.com>
2024-03-27 00:15:24 +0700

    edit authopttions

--- src/libs/createBooking.tsx
+++ src/libs/createBooking.tsx
@@ -1,18 +1,29 @@
-export default async function createBooking(carId:string, providerId:string, startDate:Date, endDate:Date) {
-    const response = await fetch(`http://localhost:8000/cars/${carId}/login`,{
-        method: "POST",
-        headers:{
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            ProviderID: providerId,
-            StartDate: startDate,
-            EndDate: endDate
-        }),
-    })
-    if(!response.ok){
-        throw new Error("Failed to add Booking")
-    }
+"use server";
+import { authOptions } from "@/libs/auth";
+import { getServerSession } from "next-auth";
 
-    return await response.json()
+export default async function createBooking(
+  carId: string,
+  providerId: string,
+  startDate: Date,
+  endDate: Date
+) {
+  const session = await getServerSession(authOptions);
+  const response = await fetch(`http://localhost:8000/cars/${carId}/bookings`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${session?.user?.token}`,
+    },
+    body: JSON.stringify({
+      ProviderID: providerId,
+      StartDate: startDate,
+      EndDate: endDate,
+    }),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to add Booking");
+  }
+
+  return await response.json();
 }
