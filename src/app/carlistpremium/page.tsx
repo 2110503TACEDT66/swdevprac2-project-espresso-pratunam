75696b76ecf7b83953144222c13a20cc01b8e9a9
gg05t <144322678+gg05t@users.noreply.github.com>
2024-03-26 23:30:36 +0700

    premium edit

--- src/app/carlistpremium/page.tsx
+++ src/app/carlistpremium/page.tsx
@@ -1,30 +1,53 @@
 import CarCard from "@/components/carCard";
 import RecommendCard from "@/components/recommendCard";
 import TopPremium from "@/components/toppremium";
+import getAllCars from "@/libs/getAllCars";
 import Link from "next/link";
+import { Car } from "@/interface/getAllCarsInterface";
 
 const mockCars = [
   {
     name: "Honda Civic",
-    imgSrc: "/img/recommendCivic.jpg",
-    zeroToHundred: 8.5,
-    topSpeed: 137,
-    engineLitre: 1.5,
+        brand: "Honda",
+        model: "Civic",
+        year: 2022,
+        color: "Silver",
+        type: "Sedan",
+        registrationNumber: "ABC123",
+        price: 5000,
+        imgSrc: "/img/recommendCivic.jpg",
+        zeroToHundred: 8.5,
+        topSpeed: 137,
+        engineLitre: 1.5,
   },
 
   {
     name: "Tesla Model 3",
-    imgSrc: "/img/recommendModel3.jpg",
-    zeroToHundred: 3.1,
-    topSpeed: 162,
-    engineLitre: 0,
+        brand: "Tesla",
+        model: "Model 3",
+        year: 2021,
+        color: "Red",
+        type: "Other",
+        registrationNumber: "DEF456",
+        price:8000,
+        imgSrc: "/img/recommendModel3.jpg",
+        zeroToHundred: 3.1,
+        topSpeed: 162,
+        engineLitre: 0,
   },
   {
     name: "Ford Mustang GT",
-    imgSrc: "/img/recommendMustang.jpg",
-    zeroToHundred: 4.9,
-    topSpeed: 180,
-    engineLitre: 5.0,
+        brand: "Ford",
+        model: "Mustang GT",
+        year: 2019,
+        color: "Blue",
+        type: "Other",
+        registrationNumber: "GHI789",
+        price:22000,
+        imgSrc: "/img/recommendMustang.jpg",
+        zeroToHundred: 4.9,
+        topSpeed: 180,
+        engineLitre: 5.0,
   },
 ];
 
@@ -37,6 +60,7 @@ export const carData = [
         color: "Silver",
         type: "Sedan",
         registrationNumber: "ABC123",
+        price:5000,
         imgSrc: "/img/recommendCivic.jpg",
         zeroToHundred: 8.5,
         topSpeed: 137,
@@ -51,6 +75,7 @@ export const carData = [
         color: "Red",
         type: "Other",
         registrationNumber: "DEF456",
+        price:8000,
         imgSrc: "/img/recommendModel3.jpg",
         zeroToHundred: 3.1,
         topSpeed: 162,
@@ -64,6 +89,7 @@ export const carData = [
         color: "Blue",
         type: "Other",
         registrationNumber: "GHI789",
+        price:22000,
         imgSrc: "/img/recommendMustang.jpg",
         zeroToHundred: 4.9,
         topSpeed: 180,
@@ -77,6 +103,7 @@ export const carData = [
         color: "White",
         type: "Sedan",
         registrationNumber: "JKL012",
+        price:5000,
         imgSrc: "/img/toyota-camry.jpg",
         zeroToHundred: 8.0, // Approximate
         topSpeed: 135,
@@ -90,6 +117,7 @@ export const carData = [
         color: "Silver",
         type: "Sedan",
         registrationNumber: "MNO345",
+        price:5000,
         imgSrc: "/img/honda-accord.jpg",
         zeroToHundred: 7.5, // Approximate
         topSpeed: 130,
@@ -103,6 +131,7 @@ export const carData = [
         color: "Black",
         type: "Sedan",
         registrationNumber: "PQR678",
+        price:5000,
         imgSrc: "/img/nissan-altima.jpg",
         zeroToHundred: 7.8, // Approximate
         topSpeed: 128,
@@ -116,6 +145,7 @@ export const carData = [
         color: "Red",
         type: "Sedan",
         registrationNumber: "STU901",
+        price:11000,
         imgSrc: "/img/hyundai-sonata.jpg",
         zeroToHundred: 8.2, // Approximate
         topSpeed: 130,
@@ -129,6 +159,7 @@ export const carData = [
         color: "Blue",
         type: "Sedan",
         registrationNumber: "VWX234",
+        price:15000,
         imgSrc: "/img/volkswagen-passat.jpg",
         zeroToHundred: 8.5,  // Approximate
         topSpeed: 126,
@@ -136,15 +167,19 @@ export const carData = [
       },
 ]
 
-const CarListPage = () => {
+
+const CarListPage = async () => {
+const carFetched = await getAllCars()
+
+const carList = carFetched['data'];
+
   return (
     <main className="relative bg-white pb-5">
       <TopPremium></TopPremium>
-      <div className=" w-screen ">
+      <div className=" w-screen mx-auto my-20">
         <div className="w-full h-[60vh] grid grid-flow-col grid-col-3 gap-0">
           {mockCars.map((car) => {
             return (
-              
               <RecommendCard
                 key={car.name}
                 name={car.name}
@@ -152,22 +187,19 @@ const CarListPage = () => {
                 zeroToHundred={car.zeroToHundred}
                 topSpeed={car.topSpeed}
                 engineLitre={car.engineLitre}
-                
+
               ></RecommendCard>
-          
-              
+
+
             );
           })}
         </div>
         <h1 className="text-black text-4xl font-semibold px-10 mt-5 mb-5">Available Car</h1>
         <div className="w-full grid grid-cols-4 gap-8 px-20 mb-10">
         {
-            carData.map((car) => {
+            carList.map((car:Car) => {
                 return(
-                   <Link href={`/cardetailpremium/${car.name}`}>
-                     <CarCard key={car.name} name={car.name} brand={car.brand} model={car.model} year={car.year} color={car.color} type={car.type} registrationNumber= {car.registrationNumber} imgSrc={car.imgSrc} zeroToHundred={car.zeroToHundred} topSpeed={car.topSpeed} engineLitre={car.engineLitre}></CarCard>
-                  </Link>
-                   
+                     <CarCard key={car.RegistrationNumber} name={car.Model} brand={car.Brand} model={car.Model} year={car.Year} color={car.Color} type={car.Type} registrationNumber= {car.RegistrationNumber} imgSrc={car.imgsrc} zeroToHundred={car.zerotohundred} topSpeed={car.topspeed} engineLitre={car.enginelitre} carId={car._id}></CarCard>
                 )
             })
         }
---
+++ src/components/carCardpre.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  name: string;
+  imgSrc: string;
+  brand: string;
+  model: string;
+  year: number;
+  color: string;
+  type: string;
+  price: number;
+  registrationNumber: string,
+  zeroToHundred: number;
+  topSpeed: number;
+  engineLitre: number;
+  carId:string
+}
+
+const CarCard = ({
+  name,
+  imgSrc,
+  zeroToHundred,
+  topSpeed,
+  engineLitre,
+  registrationNumber,
+  carId
+}: Props) => {
+  return (
+    <Link href={`/cardetailpremium/${carId}`}>
+    <div className="w-[100%] h-[35vh] bg-white rounded-lg shadow-md hover:shadow-[0_0_60px_10px_rgba(0,0,0,0.1)]">
+      <div className="relative w-full h-[70%] rounded-lg">
+        <Image
+          src={imgSrc}
+          layout="fill"
+          objectFit="cover"
+          alt={`${imgSrc}`}
+          objectPosition="center"
+          className="rounded-t-lg"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center h-[30%]">
+        <h2 className="text-xl font-semibold text-black text-center mb-1">{name}</h2>
+        <div className="text-black text-sm flex flex-row justify-between w-full">
+          <div className="text-center w-[30%]">
+            <h3 className="font-bold text-md">{zeroToHundred} s</h3>
+            <p className="text-s font-light">0-100</p>
+          </div>
+          <div className="w-0.5 bg-white h-full"></div>
+          <div className="text-center w-[30%]">
+            <h3 className="font-bold text-md">
+              {topSpeed} <span className="text-xs">mph</span>
+            </h3>
+            <p className="text-s font-light">Top speed</p>
+          </div>
+
+          <div className="w-0.5 bg-white h-full"></div>
+
+          <div className="text-center w-[30%]">
+            <h3 className="font-bold text-md">{engineLitre}L</h3>
+            <p className="text-s font-light">Engine</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </Link>
+
+  );
+};
+
+export default CarCard;
