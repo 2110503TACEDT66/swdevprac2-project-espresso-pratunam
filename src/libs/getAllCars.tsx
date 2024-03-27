25dccb349cff2256f8e612722c47b7dc53fec38f
VizKey01 <micmin0101@gmail.com>
2024-03-27 21:31:54 +0700

    fix 5

--- src/libs/getAllCars.tsx
+++ src/libs/getAllCars.tsx
@@ -1,18 +1,18 @@
-// 'use server'
+'use server'
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth";
 
 export default async function getAllCars() {
-    const session = await getServerSession(authOptions);
-    const response = await fetch("http://localhost:8000/cars",{
-        method: "GET",
-        headers:{
-            "Content-Type": "application/json",
-            authorization: `Bearer ${session?.user?.token}`,
-        },
-    })
-    if(!response.ok){
-        throw new Error("Failed to fetch Cars")
-    }
-    return await response.json()
+    // const session = await getServerSession(authOptions);
+    // const response = await fetch("http://localhost:8000/cars",{
+    //     method: "GET",
+    //     headers:{
+    //         "Content-Type": "application/json",
+    //         authorization: `Bearer ${session?.user?.token}`,
+    //     },
+    // })
+    // if(!response.ok){
+    //     throw new Error("Failed to fetch Cars")
+    // }
+    // return await response.json()
 }
